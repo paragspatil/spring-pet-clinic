7ef404ff7cd722e60eef9f36fbda57c14693a5bf
paragspatil <paragspatil123@gmail.com>
2021-01-10 12:45:12 +0530

    added speciality entity, added to vets

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinic.model;
+
+public class Speciality extends BaseEntity{
+    private String discription;
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
