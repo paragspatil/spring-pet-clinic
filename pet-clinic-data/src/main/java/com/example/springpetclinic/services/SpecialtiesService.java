c8e5187fd7a5b868dfe9118c04db5689a4a9022e
paragspatil <paragspatil123@gmail.com>
2021-01-10 16:24:48 +0530

    added specialties map impl

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.example.springpetclinic.services;
+
+import com.example.springpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+
+}
