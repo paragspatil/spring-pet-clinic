17301357336b583cb7bf0b4a5ec02172bca21172
paragspatil <paragspatil123@gmail.com>
2021-01-19 18:26:21 +0530

    added project lombok

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/PetType.java
@@ -1,18 +1,18 @@
 package com.example.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
     private String name;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
